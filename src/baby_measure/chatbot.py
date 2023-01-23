690ce782906aec5db4c1449c65244a11ff742d4f
antarcticrainforest <martin.bergemann@posteo.org>
2023-01-23 16:06:10 +0100

    Som bug fixing

--- src/baby_measure/chatbot.py
+++ src/baby_measure/chatbot.py
@@ -146,12 +146,8 @@ class ChatBot(Resource):
         total_amount_fig = self.plot.amount
         daily_amount_fig = self.plot.daily_amount
         breastfeeding_fig = self.plot.breastfeeding
-        weight_fig = self.plot.plot_body(
-            "weight", "Weight [km]", "Body Weight"
-        )
-        height_fig = self.plot.plot_body(
-            "height", "Height [cm]", "Body Height"
-        )
+        weight_fig = self.plot.plot_body("weight", "Weight [km]", "Body Weight")
+        height_fig = self.plot.plot_body("height", "Height [cm]", "Body Height")
         head_fig = self.plot.plot_body("head", "Size [cm]", "Head Size")
         nappy_fig = self.plot.nappy
         self.plot.save_plots(
@@ -160,7 +156,7 @@ class ChatBot(Resource):
             breastfeeding_fig,
             weight_fig,
             height_fig,
-            height_fig,
+            head_fig,
             nappy_fig,
         )
         if self.gh_page.use_gh_pages and self.gh_page._item_queue.empty():
@@ -236,7 +232,7 @@ class ChatBot(Resource):
                 ctype = "pee"
             df = pd.DataFrame({"id": [idx], "time": [time], "type": [ctype]})
 
-        self.append_db(table, df)
+        self.db_settings.append_db(table, df)
         self._save_and_commit()
         columns = [c for c in df.columns if c != "id"]
         df["time"] = df["time"].dt.strftime("%a %_d. %b %R")
@@ -274,14 +270,8 @@ class ChatBot(Resource):
         time = last["time"].strftime("%a %_d. %b %R")
         if table == "body":
             return f"Measures from {time}:\n{last[['weight', 'height', 'head']].to_string()}"
-        daily = (
-            entries.groupby(pd.Grouper(freq="1D"))
-            .sum(numeric_only=True)
-            .loc[day]
-        )
-        amount_key = [
-            c for c in last.keys() if c not in ("id", "uid", "time")
-        ][0]
+        daily = entries.groupby(pd.Grouper(freq="1D")).sum(numeric_only=True).loc[day]
+        amount_key = [c for c in last.keys() if c not in ("id", "uid", "time")][0]
         try:
             amount = float(last[amount_key])
             daily_amount = float(daily[amount_key])
@@ -289,13 +279,13 @@ class ChatBot(Resource):
                 daily_values = ""
             else:
                 daily_values = f" (sum that day: {daily_amount})"
-            return (
-                f"The {content} amount from {time} was {amount} {daily_values}"
-            )
+            return f"The {content} amount from {time} was {amount} {daily_values}"
         except ValueError:
             amount = last[amount_key]
             daily_values = int(daily["count"])
-        return f"On {time} the nappy content was {amount} (total: {daily_values} nappies)"
+        return (
+            f"On {time} the nappy content was {amount} (total: {daily_values} nappies)"
+        )
 
     def _process_text(self, text: str):
         """Extract the instructions from a text."""
